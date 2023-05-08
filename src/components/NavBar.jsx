ae66b8f153c1267130c3d7f8bb088d4ffe009b59
unknown <hughesgeorgeking@gmail.com>
2023-05-08 21:18:22 +0000

    changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -199,7 +199,7 @@ const NavBar = () => {
       {showMenu && (
         <motion.div
           animate={{ opacity: 1, y: 0 }}
-          initial={{ opacity: 0, y: -500 }}
+          initial={{ opacity: 0, y: -200 }}
           className="fixed w-full h-[350px] top-0 left-0 right-0 bg-white md:hidden z-30"
         >
           <div className="flex items-center justify-between px-5 py-3 bg-white">
