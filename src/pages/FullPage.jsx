8ad8f9d02d2cb23e4551113efb185987d55a077d
unknown <hughesgeorgeking@gmail.com>
2023-05-25 09:38:16 +0000

    changes

--- src/pages/FullPage.jsx
+++ src/pages/FullPage.jsx
@@ -14,19 +14,23 @@ const FullPage = () => {
       <NavBar />
 
       <div className="container mx-auto mt-10">
-        <div className="w-full md:w-2/3">
-          <div className="w-full">
-            <div className="w-full h-[420px] relative overflow-hidden bg-gray-50 rounded-md flex items-center justify-center group">
+        <div className="w-full flex flex-col md:flex-row">
+          <div className="w-full md:w-2/3">
+            <div className="w-full h-[320px] relative overflow-hidden bg-gray-50 rounded-md flex items-center justify-center md:h-[450px] group">
               <img
                 src={"../" + image}
                 alt=""
-                className="absolute h-full rounded-t-lg group-hover:transform group-hover:rotate-6 group-hover:scale-125 duration-500"
+                className="absolute h-full rounded-t-lg group-hover:transform group-hover:scale-150 duration-500"
               />
             </div>
             <h2 className="text-pink-600 text-xl font-medium mt-8">#Online</h2>
             <h1 className="text-2xl text-center font-medium mt-3 md:text-5xl">
               {title}
             </h1>
+          </div>
+
+          <div className="w-full h-fit mt-12 md:w-1/3 md:mt-0 rounded-lg shadow-md">
+            <h1 className="text-xl font-medium">Event details</h1>
           </div>
         </div>
       </div>
