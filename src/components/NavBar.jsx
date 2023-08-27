6b66650c8ea1ecec7c629bee188acede82b77ade
unknown <hughesgeorgeking@gmail.com>
2023-08-27 16:13:26 +0000

    changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { logo } from "./../assets";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import {
   FaFacebookSquare,
   FaTwitterSquare,
@@ -13,19 +13,42 @@ import { MdLocationPin } from "react-icons/md";
 import { HiMenuAlt2 } from "react-icons/hi";
 
 const NavBar = () => {
-  const homePage = "/";
-  const aboutPage = "/about";
-  const contactPage = "/contact";
-  const getInvolvedPage = "/get_involved";
-  const teamPage = "/team";
-  const eventPage = "/event";
-  const galleryPage = "/gallery";
+  const routes = [
+    {
+      name: "Services",
+      url: "/",
+    },
+    {
+      name: "Events",
+      url: "/event",
+    },
+    {
+      name: "Gallery",
+      url: "/gallery",
+    },
+    {
+      name: "Team",
+      url: "/team",
+    },
+    {
+      name: "Get Involved",
+      url: "/get_involved",
+    },
+    {
+      name: "About",
+      url: "/about",
+    },
+    {
+      name: "Contact",
+      url: "/contact",
+    },
+  ];
 
   const [activeNav, setActiveNav] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
 
   const changeActiveNav = () => {
-    if (window.scrollY >= 250) {
+    if (window.scrollY >= 350) {
       setActiveNav(true);
     } else {
       setActiveNav(false);
@@ -37,14 +60,12 @@ const NavBar = () => {
     setShowMenu((prevState) => !prevState);
   };
 
-  // "fixed w-full shadow-sm z-30 bg-white duration-300"
-
   return (
-    <nav className="w-full shadow-sm duration-300">
+    <nav className="w-full shadow-sm duration-300 relative">
       {/* Top Side */}
       <div className="border-b-2 py-4">
-        <div className="container mx-auto flex flex-col items-center md:flex-row md:justify-between">
-          <div className="flex flex-col items-center justify-center gap-1 md:flex-row md:justify-between md:gap-5">
+        <div className="container mx-auto flex flex-col items-center lg:flex-row lg:justify-between">
+          <div className="flex flex-col items-center justify-center gap-1 lg:flex-row md:justify-between md:gap-5">
             <h3 className="flex items-center justify-center">
               <IoMdMail size={18} className="mr-2" />
               info@cancersupportnf.org
@@ -86,201 +107,145 @@ const NavBar = () => {
       {/* Bottom Side */}
       <div className=" py-3">
         <div className="container mx-auto flex items-center justify-between">
-          <Link to={homePage} className="logo">
+          <NavLink to={routes[0].url} className="logo">
             <img src={logo} alt="" />
-          </Link>
+          </NavLink>
 
-          <ul className="hidden md:flex items-center justify-center gap-5">
-            <li>
-              <Link to={homePage} className="text-xl font-normal">
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link to={eventPage} className="text-xl font-normal">
-                Events
-              </Link>
-            </li>
-            <li>
-              <Link to={galleryPage} className="text-xl font-normal">
-                Gallery
-              </Link>
-            </li>
-            <li>
-              <Link to={teamPage} className="text-xl font-normal">
-                Team
-              </Link>
-            </li>
-            <li>
-              <Link to={getInvolvedPage} className="text-xl font-normal">
-                Get Involved
-              </Link>
-            </li>
-            <li>
-              <Link to={aboutPage} className="text-xl font-normal">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to={contactPage} className="text-xl font-normal">
-                Contact
-              </Link>
-            </li>
+          <ul className="hidden lg:flex items-center justify-center gap-5">
+            {routes.map((route) => {
+              return (
+                <li key={route.name}>
+                  <NavLink
+                    to={route.url}
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-xl font-bold text-pink-600"
+                        : "text-xl font-normal"
+                    }
+                  >
+                    {route.name}
+                  </NavLink>
+                </li>
+              );
+            })}
           </ul>
 
-          <button className="md:hidden" onClick={showMenuToggle}>
+          <button className="lg:hidden" onClick={showMenuToggle}>
             <HiMenuAlt2 size={30} />
           </button>
 
-          <button className="hidden text-xl md:block border-2 border-pink-400 rounded-md px-5 py-2 hover:text-white hover:bg-pink-400 hover:border-gray-600 transition-all duration-300">
+          <button className="hidden text-xl lg:block border-2 border-pink-400 rounded-md px-5 py-2 hover:text-white hover:bg-pink-400 hover:border-gray-600 transition-all duration-300">
             Support Us
           </button>
         </div>
       </div>
 
       {/* Bottom Side with Active */}
-      {activeNav && (
-        <div className="fixed top-0 right-0 left-0 w-full py-3 bg-white z-20 shadow-md">
-          <div className="container mx-auto flex items-center justify-between">
-            <Link to={homePage} className="logo">
-              <img src={logo} alt="" />
-            </Link>
+      <AnimatePresence>
+        {activeNav && (
+          <motion.div
+            initial={{ opacity: 0, top: -400 }}
+            animate={{ opacity: 1, top: 0 }}
+            exit={{ opacity: 0, top: -400 }}
+            transition={{ duration: 0.3 }}
+            className="fixed top-0 right-0 left-0 w-full py-3 bg-white z-20 shadow-md"
+          >
+            <div className="container mx-auto flex items-center justify-between">
+              <NavLink to={routes[0].url} className="logo">
+                <img src={logo} alt="" />
+              </NavLink>
 
-            <ul className="hidden md:flex items-center justify-center gap-5">
-              <li>
-                <Link to={homePage} className="text-xl font-normal">
-                  Services
-                </Link>
-              </li>
-              <li>
-                <Link to={eventPage} className="text-xl font-normal">
-                  Events
-                </Link>
-              </li>
-              <li>
-                <Link to={galleryPage} className="text-xl font-normal">
-                  Gallery
-                </Link>
-              </li>
-              <li>
-                <Link to={teamPage} className="text-xl font-normal">
-                  Team
-                </Link>
-              </li>
-              <li>
-                <Link to={getInvolvedPage} className="text-xl font-normal">
-                  Get Involved
-                </Link>
-              </li>
-              <li>
-                <Link to={aboutPage} className="text-xl font-normal">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link to={contactPage} className="text-xl font-normal">
-                  Contact
-                </Link>
-              </li>
-            </ul>
+              <ul className="hidden lg:flex items-center justify-center gap-5">
+                {routes.map((route) => {
+                  return (
+                    <li key={route.name}>
+                      <NavLink
+                        to={route.url}
+                        className={({ isActive }) =>
+                          isActive
+                            ? "text-xl font-bold text-pink-600"
+                            : "text-xl font-normal"
+                        }
+                      >
+                        {route.name}
+                      </NavLink>
+                    </li>
+                  );
+                })}
+              </ul>
 
-            <button className="md:hidden" onClick={showMenuToggle}>
-              <HiMenuAlt2 size={30} />
-            </button>
+              <button className="lg:hidden" onClick={showMenuToggle}>
+                <HiMenuAlt2 size={30} />
+              </button>
 
-            <button className="hidden text-xl md:block border-2 border-pink-400 rounded-md px-5 py-2 hover:text-white hover:bg-pink-400 hover:border-gray-600 transition-all duration-300">
-              Support Us
-            </button>
-          </div>
-        </div>
-      )}
+              <button className="hidden text-xl lg:block border-2 border-pink-400 rounded-md px-5 py-2 hover:text-white hover:bg-pink-400 hover:border-gray-600 transition-all duration-300">
+                Support Us
+              </button>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       {/* Show hidden Menu */}
-      {showMenu && (
-        <motion.div
-          animate={{ opacity: 1, y: 0, scale: 1 }}
-          initial={{ opacity: 0, y: -200, scale: 0 }}
-          className="fixed w-full h-[350px] top-0 left-0 right-0 bg-white md:hidden z-30"
-        >
-          <div className="flex items-center justify-between px-5 py-3 bg-white">
-            <Link to={homePage}>
-              <img src={logo} className="cursor-pointer" />
-            </Link>
-            <IoMdClose
-              size={30}
-              onClick={showMenuToggle}
-              className="cursor-pointer"
-            />
-          </div>
-          <div className="w-full h-full bg-white relative -my-3">
-            <ul className="w-full h-full bg-white flex flex-col items-center justify-center">
-              <li className="mb-4 text-start mx-1 px-2 duration-200 mt-5">
-                <Link
-                  to={homePage}
-                  className="text-lg font-normal px-2 border-b-2 "
-                >
-                  Services
-                </Link>
-              </li>
-              <li className="mb-4 text-start mx-1 px-2 duration-200">
-                <Link
-                  to={eventPage}
-                  className="text-lg font-normal px-2 border-b-2 "
-                >
-                  Events
-                </Link>
-              </li>
-              <li className="mb-4 text-start mx-1 px-2 duration-200">
-                <Link
-                  to={galleryPage}
-                  className="text-lg font-normal px-2 border-b-2 "
-                >
-                  Gallery
-                </Link>
-              </li>
-              <li className="mb-4 text-start mx-1 px-2 duration-200">
-                <Link
-                  to={teamPage}
-                  className="text-lg font-normal px-2 border-b-2 "
-                >
-                  Team
-                </Link>
-              </li>
-              <li className="mb-4 text-start mx-1 px-2 duration-200">
-                <Link
-                  to={getInvolvedPage}
-                  className="text-lg font-norma px-2 border-b-2 "
-                >
-                  Get Involved
-                </Link>
-              </li>
-              <li className="mb-4 text-start mx-1 px-2 duration-200">
-                <Link
-                  to={aboutPage}
-                  className="text-lg font-normal px-2 border-b-2 "
-                >
-                  About
-                </Link>
-              </li>
-              <li className="mb-4 text-start mx-1 px-2 duration-200">
-                <Link
-                  to={contactPage}
-                  className="text-lg font-normal px-2 border-b-2 "
-                >
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </motion.div>
-      )}
+      <AnimatePresence>
+        {showMenu && (
+          <motion.div
+            initial={{ opacity: 0, right: -500 }}
+            animate={{ opacity: 1, right: 0 }}
+            exit={{ opacity: 0, right: -500 }}
+            transition={{ duration: 0.3 }}
+            className="fixed w-2/3 h-full top-0 right-0 bg-white lg:hidden z-30"
+          >
+            <div className="flex items-center justify-between px-5 bg-white py-4">
+              <NavLink to={routes[0].url}>
+                <img src={logo} className="cursor-pointer" />
+              </NavLink>
+              <IoMdClose
+                size={30}
+                onClick={showMenuToggle}
+                className="cursor-pointer"
+              />
+            </div>
+            <div className="w-full h-full bg-white relative">
+              <ul className="w-full h-[70%] bg-white flex flex-col items-center justify-center">
+                {routes.map((route) => {
+                  return (
+                    <li
+                      className="mb-2 text-start mx-1 px-2 duration-200 mt-3"
+                      key={route.name}
+                    >
+                      <NavLink
+                        to={route.url}
+                        className={({ isActive }) =>
+                          isActive
+                            ? "text-lg text-pink-600 font-bold"
+                            : "text-lg font-normal px-2"
+                        }
+                        onClick={() => setShowMenu(false)}
+                      >
+                        {route.name}
+                      </NavLink>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
 
-      {showMenu && (
-        <div
-          className="fixed z-20 w-full h-full left-0 right-0 bottom-0 bg-gradient-to-br from-[rgba(0,0,0,0.6)] to-[rgba(0,0,0,0.8)] backdrop-blur-[1.5px] md:hidden"
-          onClick={showMenuToggle}
-        ></div>
-      )}
+      <AnimatePresence>
+        {showMenu && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="fixed z-20 w-full h-full left-0 right-0 bottom-0 bg-gradient-to-br from-[rgba(0,0,0,0.8)] to-[rgba(0,0,0,0.9)] backdrop-blur-[2px] lg:hidden"
+            onClick={showMenuToggle}
+          />
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
