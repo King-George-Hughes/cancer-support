5410e16678cf365632d3d93a1718f516d260ec34
unknown <hughesgeorgeking@gmail.com>
2023-09-10 21:30:10 +0000

    changes

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -2,10 +2,12 @@ import { FaWhatsapp } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const WhatsApp = () => {
-  const whatsApp = "https://api.whatsapp.com/send?phone=0552689014";
+  // const whatsApp = "https://api.whatsapp.com/send?phone=0552689014";
+  // const whatsApp = "whatsapp://send?phone=0552689014";
+  const whatsApp = "https://wa.me/+233552689014";
 
   return (
-    <Link to={whatsApp} target="_blank">
+    <Link to={whatsApp} target="_blank" rel="noopener noreferrer">
       <div className="w-12 h-12 bg-green-600 rounded-full flex items-center justify-center fixed z-50 bottom-5 right-5 hover:transform hover:-translate-y-3 hover:scale-125 duration-500">
         <FaWhatsapp size={30} className="text-green-900" />
       </div>
