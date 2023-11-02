af9c3a620f16f797861ac70b4da5c73a7d05803b
King-George-Hughes <hughesgeorgeking@gmail.com>
2023-11-02 16:34:51 +0000

    changes

--- src/pages/RecentNews.jsx
+++ src/pages/RecentNews.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { latestNews } from "../data/data";
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from "react-share";
+
+import {
+  EmailIcon,
+  FacebookIcon,
+  LinkedinIcon,
+  TelegramIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from "react-share";
 
 const RecentNews = () => {
   const { recent_id, recent_title } = useParams();
@@ -8,6 +25,8 @@ const RecentNews = () => {
     (data) => data.id === +recent_id && data.title === recent_title
   );
   const { title, gallery, message } = recentArr;
+
+  const currentPageUrl = window.location.href;
 
   return (
     <div className="container mx-auto mt-10">
@@ -31,6 +50,32 @@ const RecentNews = () => {
           }
           return <p className="text-gray-500 my-1 max-w-[850px]">{msg}</p>;
         })}
+
+        <p className="inline-flex items-center justify-start gap-2">
+          <FacebookShareButton
+            url={currentPageUrl}
+            quote="Please share this post"
+            hashtag={title}
+          >
+            <FacebookIcon size={32} round={true} />
+          </FacebookShareButton>
+
+          <WhatsappShareButton
+            url={currentPageUrl}
+            quote="Please share this post"
+            hashtag={title}
+          >
+            <WhatsappIcon size={32} round={true} />
+          </WhatsappShareButton>
+
+          <TelegramShareButton
+            url={currentPageUrl}
+            quote="Please share this post"
+            hashtag={title}
+          >
+            <TelegramIcon size={32} round={true} />
+          </TelegramShareButton>
+        </p>
       </div>
       <div className="w-full h-full flex flex-col md:flex-row md:flex-wrap">
         {gallery.map((gal) => (
